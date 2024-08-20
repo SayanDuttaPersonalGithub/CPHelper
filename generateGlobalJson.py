bca032fc09f0c71f1a68d59951caeb7e7a207a86
doesnotexisits <sayandutta995@gmail.com>
2024-08-20 14:23:04 +0530

    ws

--- generateGlobalJson.py
+++ generateGlobalJson.py
@@ -74,20 +74,12 @@ freopen("Error.txt", "w", stderr);
 	return 0;
 }
 '''
->>>>>>> c435abe4889a4d098824a35d86d23177eb407c36
-
 import json
-
 D = {}
 D['template'] = TEMPLATE
 D['inputTxtPath'] = "C:\Users\SAYAN\AppData\Roaming\Sublime Text\Packages\User\input.txt"
 D['dashCount'] = 50
 D['baseContestPath'] = "C:\Users\SAYAN\AppData\Roaming\Sublime Text\Packages\User"
-<<<<<<< HEAD
-D['author'] = 'Puneet Rai'
-=======
-D['author'] = 'Sayan'
->>>>>>> c435abe4889a4d098824a35d86d23177eb407c36
 D['teamName'] = 'BlundersPride'
 
 with open('globals.json' , 'wb') as glob:
